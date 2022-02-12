b83d4c0ee74a261979aea1e6843372bc6e04dc16
DivyaPrabhaShan <98879001+DivyaPrabhaShan@users.noreply.github.com>
2022-02-12 20:54:17 +0530

    Add files via upload

---
+++ 3_Implementation/main.h
@@ -0,0 +1,18 @@
+/**
+ * @file movieticket.h
+ * @author DivyPrabhaShan (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-12
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+//headerground
+#ifndef MOVIETICKET_H_
+#define MOVIETICKLET_H_
+//include path
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#endif 
