ebe7fdab916c3ef8fe3db49b9f8899026c48fd67
DivyaPrabhaShan <98879001+DivyaPrabhaShan@users.noreply.github.com>
2022-02-23 22:52:04 +0530

    Add files via upload

---
+++ 3_Implementation/Inc/MOVIE_TICKET.h
@@ -0,0 +1,27 @@
+/**
+ * @file MOVIE_TICKET.H
+ * @author dhivyaprabha
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef __MOVIE_TICKET_H__
+#define __MOVIE_TICKET_H__
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+/**
+ * @brief Movie ticket
+ * 
+ */
+void Enterdetails();
+void Lookall(); 
+void Find(); 
+void reserve_ticket(); 
+void past_record();
+#endif
